66cf7d892d0f8b15b513430eb03fcca63d535933
Isabella Escalona <isabellaescalona0110@gmail.com>
2026-02-15 18:19:43 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who=["Mi mama ", "Mi perro ", "Mi hermano "];
+  let action=["rompio ","mojo ","aplasto ","comio "];
+  let what=["mi bolso ","mi tarea ","mi cuaderno "];
+  let when=["mientras dormia.", "ayer cuando sali a pasear.", "mientras comia.", "cuando estaba en el gym."];
+
+  let r1=Math.floor(Math.random()*who.length);
+  let r2=Math.floor(Math.random()*action.length);
+  let r3=Math.floor(Math.random()*what.length);
+  let r4=Math.floor(Math.random()*when.length);
+//revisa que es la funcion onload plis 
+
+  document.querySelector("#excuse").innerHTML = who[r1] + action[r2] + what[r3] + when[r4];
 };
